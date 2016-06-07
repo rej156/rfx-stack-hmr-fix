f80425606ca27a9c9a99676823033fba9be597a6
Claudio Savino <savino87@gmail.com>
2016-06-07 18:03:09 +0200

    update

--- src/web/client.jsx
+++ src/web/client.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { browserHistory } from 'react-router';
-import { rehydrate } from '~/src/shared/state/hydrate';
+import { browserHistory, match } from 'react-router';
 import { AppContainer } from 'react-hot-loader';
+import { fetchDataOnLocationMatch } from '../utils/fetch';
+import { rehydrate } from '~/src/shared/state/hydrate';
 import initStore from '../shared/stores';
 import routes from '../shared/routes';
 
-console.log('test'); // eslint-disable-line no-console
 const store = rehydrate(initStore);
-
-// <App store={store} routes={routes} history={browserHistory} />
+store.ui.injectTapEv(); // material-ui fix
+fetchDataOnLocationMatch(browserHistory, routes, match, store);
 
 function renderApp(App) {
   render(
