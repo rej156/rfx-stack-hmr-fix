960a0b9f92d9b4bd2f64ab1b60425f379bc23144
Claudio Savino <savino87@gmail.com>
2016-06-08 21:28:27 +0200

    update

--- src/web/client.jsx
+++ src/web/client.jsx
@@ -8,35 +8,33 @@ import initStore from '../shared/stores';
 import routes from '../shared/routes';
 import App from './App';
 
-let store = rehydrate(initStore);
+const store = rehydrate(initStore);
 store.ui.injectTapEv(); // material-ui fix
 fetchDataOnLocationMatch(browserHistory, routes, match, store);
 
-render(
-  <AppContainer>
-    <App
-      store={store}
-      routes={routes}
-      history={browserHistory}
-    />
-  </AppContainer>,
-  document.getElementById('root')
-);
+function renderApp(AppComponent) {
+  if (window.store) {
+    window.store = require('../shared/state/store')
+      .default(JSON.parse(JSON.stringify(window.store)));
+  }
+  render(
+    <AppContainer>
+      <AppComponent
+        store={window.store || store}
+        routes={routes}
+        history={browserHistory}
+      />
+    </AppContainer>,
+    document.getElementById('root')
+  );
+}
+
+renderApp(App);
 
 if (module.hot) {
-  // if (!window.store) window.store = store;
+  if (!window.store) window.store = store;
   module.hot.accept('./App', () => {
     const NextApp = require('./App').default;
-    // renderApp(NextApp);
-    render(
-      <AppContainer>
-        <NextApp
-          store={store}
-          routes={routes}
-          history={browserHistory}
-        />
-      </AppContainer>,
-      document.getElementById('root')
-    );
+    renderApp(NextApp);
   });
 }
